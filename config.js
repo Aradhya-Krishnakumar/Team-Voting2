55917373d2bb757d8bacb1282c0f71449e3205fb
Aradhya-Krishnakumar <kkpiyali@gmail.com>
2022-08-05 23:14:55 +0530

    configup

--- config.js
+++ config.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase';
 
 // add SDK Firebase
-var firebaseConfig = {
-
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyBjumQAj1CvroQDpY6gdZlUTTkE2skeH8k",
+    authDomain: "voteteam-65320.firebaseapp.com",
+    databaseURL: "https://voteteam-65320-default-rtdb.firebaseio.com",
+    projectId: "voteteam-65320",
+    storageBucket: "voteteam-65320.appspot.com",
+    messagingSenderId: "238590279734",
+    appId: "1:238590279734:web:34ac262d930b27f9d4d897"
+  };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
